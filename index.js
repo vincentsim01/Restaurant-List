b10099b60a7a78f5cfe1b4a395567d2c4a5e26dc
Vincent Kiathadi <vincentkiathadi@gmail.com>
2023-12-05 14:54:28 +0800

    filter

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
+var pointEntry=document.getElementById("pointEntry");
+
+
 var restaurantList = [
     {
         name:"Restaurant A",
         address:"Street A District A City A State A Province A",
-        city:""
+        city:"A",
+        price:1000
 
     },
     {
         name:"Restaurant B",
         address:"Street B District B City B State B Province B",
-        city:""
+        city:"A",
+        price:2000
 
     },
     {
         name:"Restaurant C",
         address:"Street C District C City C State C Province C",
-        city:""
+        city:"C",
+        price:4000
 
     },
     {
         name:"Restaurant D",
         address:"Street D District D City D State D Province D",
-        city:""
+        city:"B",
+        price:500
+        
 
     },
     {
         name:"Restaurant E",
         address:"Street E District E City E State E Province E",
-        city:""
+        city:"B",
+        price:3000
 
     }
 ];
@@ -36,8 +45,41 @@ var restaurantList = [
 
 
 function printAllRestaurantNames(){
-    restaurantList.forEach((resto)=>console.log(resto));
+    restaurantList.forEach((resto)=>{
 
+        console.log(resto);
+
+
+        }
+
+    )
 }
 
-printAllRestaurantNames();
+// printAllRestaurantNames();
+
+
+function filterRestaurantByCity(clausa){
+    var filteredRestaurants=restaurantList.filter((ristorante)=>{
+        return ristorante.city.includes(clausa);
+        } 
+     );
+
+     return filteredRestaurants;
+     
+};
+
+var filtertheRestaurant=filterRestaurantByCity("C");
+
+console.log(filtertheRestaurant);
+
+
+// var bena=restaurantList.filter((ristorante)=>{return ristorante.city=="A";});
+
+
+// var benzi=restaurantList.filter((ristorante)=>{return ristorante.price<3000;});
+
+// console.log(bankai);
+
+// console.log(benzi);
+
+// console.log(bena);
